850965d4818631349a47cd35f65b391a81511738
GoukiHino <g.hino@i5net.jp>
2023-07-19 14:14:12 +0900

    add: component TodoForm

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,12 +1,38 @@
-import React from "react";
-import {Container} from "@mui/material";
+import React, {useState} from "react";
+import {Box, Container, Fab, Modal} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
 
 const TodoContainer = () => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        <Container component="main" maxWidth="xs">
+        <Container component="main" maxWidth="sm">
             <TodoList/>
+            <Fab color="primary" sx={{position: "absolute", bottom: 25, right: 25}} onClick={handleOpen}>
+                <AddIcon/>
+            </Fab>
+            <Modal open={open} onClose={handleClose}>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        width: 500,
+                        bgcolor: "background.paper",
+                        p: 3,
+                        borderRadius: "5px"
+                }}
+                >
+                    <TodoForm handleClose={handleClose}/>
+                </Box>
+            </Modal>
         </Container>
     );
 };
